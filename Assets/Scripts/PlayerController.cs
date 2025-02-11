9eb9bdb22960b7dcd53bcd11df3193e5180f8adc
Illya Shkorubs'ky <illyashkorubsky@hotmail.com>
2025-02-11 07:51:20 +0000

    created a line renderer to show circle around player for range identification

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 namespace Scripts
 {
+    [RequireComponent(typeof(LineRenderer))]
     public class PlayerController : PlayerManager
     {
         [SerializeField] private GameManager gameManager;
@@ -16,10 +17,12 @@ namespace Scripts
         private void Start()
         {
             myAnimator = gameObject.GetComponent<Animator>();
+            
         }
 
         private void Update()
         {
+            DoRenderer();
             if (currentTarget == null)
             {
                 currentTarget = GameManager.Instance.GetClosestEnemy();
@@ -50,6 +53,40 @@ namespace Scripts
             var direction = transform.position - currentTarget.transform.position;
             //move projectile to enemy
             myProjectile.GetComponent<Rigidbody>().AddForce(-direction * projectileSpeed, ForceMode.Force);
+        }
+
+        [Range(0, 50)]
+        public int segments = 20;
+        [Range(0, 5)]
+        public float xradius = 5;
+        [Range(0, 5)]
+        public float yradius = 5;
+        LineRenderer line;
+
+        private void DoRenderer()
+        {
+            line = gameObject.GetComponent<LineRenderer>();
+            line.positionCount = segments + 1;
+            line.useWorldSpace = false;
+            CreatePoints();
+        }
+
+        void CreatePoints()
+        {
+            float x;
+            float y;
+
+            float angle = 20f;
+
+            for (int i = 0; i < (segments + 1); i++)
+            {
+                x = Mathf.Sin(Mathf.Deg2Rad * angle) * xradius;
+                y = Mathf.Cos(Mathf.Deg2Rad * angle) * yradius;
+
+                line.SetPosition(i, new Vector3(x, 0, y));
+
+                angle += (360f / segments);
+            }
         }
     }
 }
